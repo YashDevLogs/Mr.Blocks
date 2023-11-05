96c57f1b8d1d492ee4b2badacba4c55bd8ab524b
Yash <91522919+YashDevLogs@users.noreply.github.com>
2023-11-05 19:46:24 +0530

    Added Player Controller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float speed;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (horizontalInput > 0)
+        {
+            rigidbody2D.velocity = new Vector2(speed, 0f);
+        }
+        else if (horizontalInput < 0)
+        {
+            rigidbody2D.velocity = new Vector2(-speed, 0f);
+        }
+        else if (verticalInput > 0)
+        {
+            rigidbody2D.velocity = new Vector2(0f, speed);
+        }
+        else if (verticalInput < 0)
+        {
+            rigidbody2D.velocity = new Vector2(0f, -speed);
+        }
+        else
+        {
+            rigidbody2D.velocity = new Vector2(0f, 0f);
+        }
+    }
+}
